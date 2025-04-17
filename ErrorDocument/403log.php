278cfe2989dd800170e86e96eaef0ba97928957e
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-04-17 13:05:02 +0700

    Update akses

---
+++ ErrorDocument/403log.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="./style/404.css">
+    <div class="error-container">
+        <h1>403</h1>
+        <p style="font-size:18px">Your access is forbidhen🔐<p>
+        <p>
+            <a href="./index.php?page=dashboard" class="go-back-btn">
+                Go back to the home page
+            </a>
+        </p>
+    </div>
--- asset/main.php
+++ asset/main.php
@@ -11,224 +11,224 @@
             if ($level == 'Admin' || $level == 'Owner' || $level == 'Demo') {
                 include('./users/users.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'tusers':
             if ($level == 'Admin' || $level == 'Owner' || $level == 'Demo') {
                 include('./users/tambah.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
             case 'eusers':
                 if ($level == 'Admin' || $level == 'Owner' || $level == 'Demo') {
                     include('./users/edit.php');
                 } else {
-                    echo "<h1>Akses Ditolak</h1>";
+                    include("./ErrorDocument/403log.php");
                 }
                 break;
         case 'pendapatan':
             if ($level == 'Admin' || $level == 'Owner' || $level == 'Demo') {
                 include('./pendapatan/pendapatan.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'chat':
             if ($level == 'Admin' || $level =='Owner' || $level == 'Kasir' || $level == 'Demo') {
                 include('./chat/chat.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'kategori':
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./kategori/kategori.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'tkategori':
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./kategori/kategori_tambah.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
             case 'ekategori':
                 if ($level == 'Admin' || $level == 'Demo') {
                     include('./kategori/kategori_edit.php');
                 } else {
-                    echo "<h1>Akses Ditolak</h1>";
+                    include("./ErrorDocument/403log.php");
                 }
                 break;
         case 'uk_baju':
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./kategori/uk_baju.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case'tbaju';
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./kategori/uk_baju_tambah.php');
             }else{
-                echo"<h1>Akses Ditotak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case'ebaju';
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./kategori/uk_baju_edit.php');
             } else{
-                echo '<h1>Akses Ditolak';
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'uk_celana':
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./kategori/uk_celana.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'tcelana';
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./kategori/uk_celana_tambah.php');
             } else{
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case'ecelana';
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./kategori/uk_celana_edit.php');
             }else{
-                echo"<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'stok':
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./stok/produk.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'tstok':
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./stok/produk_tambah.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'estok':
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./stok/produk_edit.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'bahan';
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./stok/bahan.php');
             } else {
-                echo "<h1>Akses Ditolak</>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'tbahan';
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./stok/bahan_tambah.php');
             } else {
-                echo "<h1>Akses Ditolak</>";
+                include("./ErrorDocument/403log.php");
             }
         break;
         case 'ebahan';
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./stok/bahan_edit.php');
             } else {
-                echo "<h1>Akses Ditolak</>";
+                include("./ErrorDocument/403log.php");
             }
         break;
         case 'custom';
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./stok/custom.php');
             } else {
-                echo "<h1>Akses Ditolak</>";
+                include("./ErrorDocument/403log.php");
             }
         break;
         case 'tcustom';
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./stok/custom_tambah.php');
             } else{
-                echo '<h1>Akses Ditolak</>';
+                include("./ErrorDocument/403log.php");
             }
         break;
         case 'ecustom';
             if ($level == 'Admin' || $level == 'Demo') {
                 include('./stok/custom_edit.php');
             }else{
-                echo '<h1>Akses Ditotal</>';
+                include("./ErrorDocument/403log.php");
             }
         break;
         case 'transaksi':
             if ($level == 'Admin' || $level == 'Kasir' || $level == 'Demo') {
                 include('./transaksi/transaksi.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
             case 'pelunasan':
                 if ($level == 'Admin' || $level == 'Kasir' || $level == 'Demo') {
                     include('./transaksi/pelunasan.php');
                 } else {
-                    echo "<h1>Akses Ditolak</h1>";
+                    include("./ErrorDocument/403log.php");
                 }
                 break;
             case 'lunas':
                 if ($level == 'Admin' || $level == 'Kasir' || $level == 'Demo') {
                     include('./kuitansi/lunas.php');
                 } else {
-                    echo "<h1>Akses Ditolak</h1>";
+                    include("./ErrorDocument/403log.php");
                 }
                 break;
             case 'lcustom':
                 if ($level == 'Admin' || $level == 'Kasir' || $level == 'Demo') {
                     include('./kuitansi/lunas_cstm.php');
                 } else {
-                    echo "<h1>Akses Ditolak</h1>";
+                    include("./ErrorDocument/403log.php");
                 }
                 break;
             case 'dcustom':
                 if ($level == 'Admin' || $level == 'Kasir' || $level == 'Demo') {
                     include('./kuitansi/dp_cstm.php');
                 } else {
-                    echo "<h1>Akses Ditolak</h1>";
+                    include("./ErrorDocument/403log.php");
                 }
                 break;
             case 'dp':
                 if ($level == 'Admin' || $level == 'Kasir' || $level == 'Demo') {
                     include('./kuitansi/dp.php');
                 } else {
-                    echo "<h1>Akses Ditolak</h1>";
+                    include("./ErrorDocument/403log.php");
                 }
                 break;
             case 'print':
                 if ($level == 'Admin' || $level == 'Kasir' || $level == 'Demo') {
                     include('./kuitansi/print_lunas.php');
                 } else {
-                    echo "<h1>Akses Ditolak</h1>";
+                    include("./ErrorDocument/403log.php");
                 }
                 break;
             case 'pcustom':
                 if ($level == 'Admin' || $level == 'Kasir' || $level == 'Demo') {
                     include('./kuitansi/print_lunas_cstm.php');
                 } else {
-                    echo "<h1>Akses Ditolak</h1>";
+                    include("./ErrorDocument/403log.php");
                 }
                 break;
         case 'laporan':
             if ($level == 'Admin' || $level == 'Owner' || $level == 'Kasir' || $level == 'Demo') {
                 include('./laporan/laporan.php');
             } else {
-                echo "<h1>Akses Ditolak</h1>";
+                include("./ErrorDocument/403log.php");
             }
             break;
         case 'settings':
